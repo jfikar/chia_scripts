3b2dec1804cd91936f34835a4a41f8ec3a16cba4
jfikar <j.fikar@gmail.com>
2021-05-20 21:18:56 +0000

    Update plot_stat.sh

--- plot_stat.sh
+++ plot_stat.sh
@@ -5,7 +5,7 @@ for j in {1..4}
 do
         i="Time for phase ${j}"
         echo -n ${i} ":"
-        grep "${i}" ${LOG_FILES} | awk '{ sumX=sumX+$6 ; sumX2+=(($6)^2)} END { printf " %.3f+-%.3f\t", sumX/(NR), sqrt((sumX2-sumX^2/NR)/(NR-1)/NR)}'
+        grep "${i}" ${LOG_FILES} | awk '{ sumX=sumX+$6 ; sumX2+=(($6)^2)} END { printf " %.3f±%.3f\t", sumX/(NR), sqrt((sumX2-sumX^2/NR)/(NR-1)/NR)}'
         echo -n " med "
         grep "${i}" ${LOG_FILES} | awk '{ print $6 }' | sort -n | awk ' { a[i++]=$1; } END { x=int((i+1)/2); if (x < (i+1)/2) print (a[x-1]+a[x])/2; else print a[x-1]; }' | sed -z '$ s/\n$//'
         echo -n " max "
@@ -16,7 +16,7 @@ done
 echo "-------------------------------------------------------------------------------------"
 i="Total time"
 echo -n ${i} ":"
-grep "${i}" ${LOG_FILES} | awk '{ sumX=sumX+$4 ; sumX2+=(($4)^2)} END { printf " %.3f+-%.3f\t", sumX/(NR), sqrt((sumX2-sumX^2/NR)/(NR-1)/NR)}'
+grep "${i}" ${LOG_FILES} | awk '{ sumX=sumX+$4 ; sumX2+=(($4)^2)} END { printf " %.3f±%.3f\t", sumX/(NR), sqrt((sumX2-sumX^2/NR)/(NR-1)/NR)}'
 echo -n " med "
 grep "${i}" ${LOG_FILES} | awk '{ print $4 }' | sort -n | awk ' { a[i++]=$1; } END { x=int((i+1)/2); if (x < (i+1)/2) print (a[x-1]+a[x])/2; else print a[x-1]; }' | sed -z '$ s/\n$//'
 echo -n " max "
